604f3611ab3b35756432dde508afc7cb69258380
현지혜 <jijinny26@gmail.com>
2025-06-04 15:38:23 +0900

    MOD: client files

--- client/client.h
+++ client/client.h
@@ -8,11 +8,23 @@
 #include <openssl/ssl.h>
 
 #define SERVER_IP "127.0.0.1"
-#define PORT 8089
-#define CLIENT_MAX_INPUT 32
+#define SSL_PORT 55014
+#define TCP_PORT 55015
+#define CLIENT_MAX_INPUT 128
 #define TOKEN_LEN 64
 
 extern SSL *ssl;
 extern char token[TOKEN_LEN];
+extern int game_sock;
+
+typedef enum {
+    SCENE_LOGIN,     // 로그인 화면
+    SCENE_SIGNUP,    // 회원가입 화면
+    SCENE_MAIN,      // 메인 화면(게임)
+    SCENE_INVALID_TOKEN,
+    SCENE_ERROR,
+    SCENE_RESULT,
+    SCENE_EXIT       // 종료
+} SceneState;
 
 #endif
